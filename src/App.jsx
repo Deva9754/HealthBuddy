70912399259e0c147b9b8d114689921536ec37eb
Deva9754 <deva.sahu2001@gmail.com>
2024-12-22 18:37:30 +0530

    add: routes added and config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Doctors from "./pages/Doctors";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MyAppointment from "./pages/MyAppointment";
+import MyProfile from "./pages/MyProfile";
+import Appointments from "./pages/Appointments";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <div className="text-green-600">
-        Hello dosto welcome back to my prok=ject
-        <Home />
+      <div className=" mx-4 sm:mx-[10%]">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/doctors" element={<Doctors />} />
+          <Route path="/doctors/:speciality" element={<Doctors />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/my-appointments" element={<MyAppointment />} />
+          <Route path="/my-profile" element={<MyProfile />} />
+          <Route path="/appointment/:docId" element={<Appointments />} />
+        </Routes>
+        <Footer />
       </div>
     </>
   );
