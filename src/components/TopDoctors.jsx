2d7b63bad8d5eb15c4a31d7ffe88e99ad6f277b0
Deva9754 <deva.sahu2001@gmail.com>
2024-12-24 11:08:14 +0530

    add: specilaity and doctors page

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -3,10 +3,14 @@ import { useNavigate } from "react-router-dom";
 
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import Shimmer from "./Shimmer";
 
 const TopDoctors = () => {
   const navigate = useNavigate();
   const { doctors } = useContext(AppContext);
+  if (!doctors) {
+    return <Shimmer />;
+  }
   return (
     <div className=" flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className=" text-3xl font-medium">Top Doctors to Book</h1>
@@ -17,7 +21,10 @@ const TopDoctors = () => {
       <div className=" w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
         {doctors.slice(0, 10).map((item, index) => (
           <div
-            onClick={() => navigate(`/appointment/${item._id}`)}
+            onClick={() => {
+              navigate(`/appointment/${item._id}`);
+              scrollTo(0, 0);
+            }}
             className=" border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
             key={index}
           >
