75249c77da50a9e0769ec84773e4f902ccfd1dc0
Deva9754 <deva.sahu2001@gmail.com>
2025-01-03 22:36:31 +0530

    remove commented routed

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,7 @@
 //     <>
 //       <div className=" mx-4 sm:mx-[10%]">
 //         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/contact" element={<Contact />} />
-//           <Route path="/doctors" element={<Doctors />} />
-//           <Route path="/doctors/:speciality" element={<Doctors />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/my-appointments" element={<MyAppointment />} />
-//           <Route path="/my-profile" element={<MyProfile />} />
-//           <Route path="/appointment/:docId" element={<Appointments />} />
-//         </Routes>
+//
 //         <Footer />
 //       </div>
 //     </>
