4a965dec1b749a9c48b0ca42aee5ca6fc0b267f3
Deva9754 <deva.sahu2001@gmail.com>
2025-04-06 22:22:33 +0530

    add: added assets import

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-// import { doctors } from "../assets/assets";
+ import { doctors } from "../assets/assets";
 
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
