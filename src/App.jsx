11c591d951a6d929f5731b38da7564ba4f12fa2f
Deva9754 <deva.sahu2001@gmail.com>
2025-04-13 22:25:27 +0530

    fixLcontact page issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import "./App.css";
 // import { Route, Routes } from "react-router-dom";
 // import About from "./pages/About";
-// import Contact from "./pages/Contact";
+// import Doctors from "./pages/Doctors";
 // import Home from "./pages/Home";
 
 import "./App.css";
