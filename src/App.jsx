81ee601071bcade5c9a965049383dea4a9397fce
Deva9754 <deva.sahu2001@gmail.com>
2025-04-13 22:25:02 +0530

    add: doctors speciality carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@
 // import { Route, Routes } from "react-router-dom";
 // import About from "./pages/About";
 // import Contact from "./pages/Contact";
-// import Doctors from "./pages/Doctors";
-
+// import Home from "./pages/Home";
 
 import "./App.css";
 import { Route, Routes, useNavigate } from "react-router-dom";
